efcbf5f8794ee081baeb357badacc00722389fd0
Antonio <antonio@querystorm.com>
2022-01-07 14:19:02 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,22 +1,16 @@
 ﻿using Newtonsoft.Json.Linq;
-using System.Diagnostics;
 using System.IO.Compression;
 using System.Text;
 using System.Xml.Linq;
 
-namespace Packer // Note: actual namespace depends on the project name.
+namespace Packer
 {
     class Program
     {
-        static Encoding encoding = Encoding.Unicode;
+        static Encoding unicode = Encoding.Unicode;
 
         static void Main(string[] args)
         {
-            Unpack(@"C:\TEST_PBI_VC\aw_sales.pbit", @"C:\TEST_PBI_VC\unpacked");
-            Pack(@"C:\TEST_PBI_VC\unpacked", @"C:\TEST_PBI_VC\aw_sales_NEW.pbit");
-
-            return;
-
             var operation = args[0];
             if (operation == "pack")
                 Pack(args[1], args[2]);
@@ -24,105 +18,78 @@ namespace Packer // Note: actual namespace depends on the project name.
             else if (operation == "unpack")
                 Unpack(args[1], args[2]);
         }
-        static void Pack(string sourceFolder, string destinationFilePath)
+        static void Pack(string sourceFolder, string outputFilePath)
         {
-            Debugger.Launch();
-            var files = Directory.GetFiles(sourceFolder, "*", SearchOption.AllDirectories);
-            foreach (var file in files)
+            var tempFolderPath = Path.Combine(Path.GetTempPath(), "Packer_temp");
+
+            if (Directory.Exists(tempFolderPath))
+                Directory.Delete(tempFolderPath, true);
+
+            foreach (var f in Directory.GetFiles(sourceFolder, "*", SearchOption.AllDirectories))
             {
-                byte[] buffer = new byte[2];
+                if (f.Contains(".git\\"))
+                    continue;
 
-                using (var fs = File.OpenRead(file))
-                    fs.Read(buffer);
+                var relativePath = f.Substring(sourceFolder.TrimEnd('\\').Length + 1);
+                var destinationPath = Path.Combine(tempFolderPath, relativePath);
 
-                if (buffer[0] == 0xFF && buffer[1] == 0xFE)
-                {
-                    var bytes = File.ReadAllBytes(file);
-                    File.WriteAllBytes(file, bytes.Skip(2).ToArray());
-                }
+                var destinationDir = Path.GetDirectoryName(destinationPath) ?? throw new Exception("Invalid path");
+                if (!Directory.Exists(destinationDir))
+                    Directory.CreateDirectory(destinationDir);
+
+                File.Copy(f, destinationPath);
             }
 
-            Directory.GetFiles(sourceFolder, "*")
+            Directory.GetFiles(tempFolderPath, "*")
                 .Where(f => string.IsNullOrEmpty(Path.GetExtension(f)) && !string.Equals(Path.GetFileName(f), "Version", StringComparison.OrdinalIgnoreCase))
-                .Union(Directory.GetFiles(Path.Combine(sourceFolder, "Report"), "*", SearchOption.AllDirectories))
+                .Union(Directory.GetFiles(Path.Combine(tempFolderPath, "Report"), "*", SearchOption.AllDirectories))
                 .ToList()
                 .ForEach(f =>
                 {
-                    var encodingToUse = string.Equals(Path.GetExtension(f), ".json", StringComparison.OrdinalIgnoreCase) ? Encoding.UTF8 : encoding;
                     var str = File.ReadAllText(f);
                     var obj = JObject.Parse(str);
                     var formatted = obj.ToString(Newtonsoft.Json.Formatting.None);
-                    var bytes = encoding.GetBytes(formatted);
+                    var bytes = unicode.GetBytes(formatted);
                     File.WriteAllBytes(f, bytes);
                 });
 
-            // ZipFile.CreateFromDirectory(sourceFolder, destinationFilePath + ".a.pbit");
-            ZipHelper.CreateFromDirectory(sourceFolder, destinationFilePath, CompressionLevel.Optimal, true, f => !f.Contains(".git"));
-        }
+            if(File.Exists(outputFilePath))
+                File.Delete(outputFilePath);
+            ZipFile.CreateFromDirectory(tempFolderPath, outputFilePath);
 
-        public static class ZipHelper
-        {
-            public static void CreateFromDirectory(
-                string sourceDirectoryName
-                , string destinationArchiveFileName
-                , CompressionLevel compressionLevel
-                , bool includeBaseDirectory
-                , Predicate<string> filter // Add this parameter
-            )
-            {
-                if (string.IsNullOrEmpty(sourceDirectoryName))
-                {
-                    throw new ArgumentNullException("sourceDirectoryName");
-                }
-                if (string.IsNullOrEmpty(destinationArchiveFileName))
-                {
-                    throw new ArgumentNullException("destinationArchiveFileName");
-                }
-                var filesToAdd = Directory.GetFiles(sourceDirectoryName, "*", SearchOption.AllDirectories);
-                using (var zipFileStream = new FileStream(destinationArchiveFileName, FileMode.Create))
-                {
-                    using (var archive = new ZipArchive(zipFileStream, ZipArchiveMode.Create))
-                    {
-                        for (int i = 0; i < filesToAdd.Length; i++)
-                        {
-                            // Add the following condition to do filtering:
-                            if (!filter(filesToAdd[i]))
-                            {
-                                continue;
-                            }
-                            var filePath = filesToAdd[i];
-                            var entry = archive.CreateEntryFromFile(filePath, filePath.Substring(sourceDirectoryName.Length + 1).Replace("\\", "/"), compressionLevel);
-                        }
-                    }
-                }
-            }
+            Directory.Delete(tempFolderPath, true);
         }
 
         static void Unpack(string file, string destinationFolder)
         {
-            foreach (var d in Directory.GetDirectories(destinationFolder))
+            if (Directory.Exists(destinationFolder))
             {
-                if (Path.GetFileName(d) == ".git")
-                    continue;
-                Directory.Delete(d, true);
+                // clear everything except the .git folder
+                foreach (var d in Directory.GetDirectories(destinationFolder))
+                {
+                    if (Path.GetFileName(d) == ".git")
+                        continue;
+                    Directory.Delete(d, true);
+                }
+                foreach (var f in Directory.GetFiles(destinationFolder))
+                {
+                    File.Delete(f);
+                }
             }
-            foreach (var f in Directory.GetFiles(destinationFolder))
-            {
-                File.Delete(f);
-            }
-            Directory.CreateDirectory(destinationFolder);
+            else
+                Directory.CreateDirectory(destinationFolder);
 
             ZipFile.ExtractToDirectory(file, destinationFolder);
 
             // strip timestamps
             HashSet<string> propertiesToStrip = new HashSet<string>() { "createdTimestamp", "modifiedTime", "structureModifiedTime", "refreshedTime" };
             var dataModelSchemaFile = Path.Combine(destinationFolder, "DataModelSchema");
-            var jobj = JObject.Parse(File.ReadAllText(dataModelSchemaFile, encoding));
+            var jobj = JObject.Parse(File.ReadAllText(dataModelSchemaFile, unicode));
             jobj.Descendants()
                 .OfType<JProperty>()
                 .Where(jp => propertiesToStrip.Contains(jp.Name))
                 .ToList().ForEach(jp => jp.Remove());
-            var bytes = encoding.GetBytes(jobj.ToString());
+            var bytes = unicode.GetBytes(jobj.ToString());
             File.WriteAllBytes(dataModelSchemaFile, bytes);
 
             // remove security bindings
@@ -137,19 +104,32 @@ namespace Packer // Note: actual namespace depends on the project name.
                 .Remove();
             doc.Save(contentTypesXmlPath);
 
-            //// reformat json files
-            //Directory.GetFiles(destinationFolder, "*")
-            //    .Where(f => string.IsNullOrEmpty(Path.GetExtension(f)) && !string.Equals(Path.GetFileName(f), "Version", StringComparison.OrdinalIgnoreCase))
-            //    .Union(Directory.GetFiles(Path.Combine(destinationFolder, "Report"), "*", SearchOption.AllDirectories))
-            //    .ToList()
-            //    .ForEach(f =>
-            //    {
-            //        var encodingToUse = string.Equals(Path.GetExtension(f), ".json", StringComparison.OrdinalIgnoreCase) ? Encoding.UTF8 : encoding;
-            //        var str = File.ReadAllText(f, encodingToUse);
-            //        var obj = JObject.Parse(str);
-            //        var formatted = obj.ToString(Newtonsoft.Json.Formatting.Indented);
-            //        File.WriteAllText(f, formatted);
-            //    });
+            // reformat json files
+            Directory.GetFiles(destinationFolder, "*")
+                .Where(f => string.IsNullOrEmpty(Path.GetExtension(f)) && !string.Equals(Path.GetFileName(f), "Version", StringComparison.OrdinalIgnoreCase))
+                .Union(Directory.GetFiles(Path.Combine(destinationFolder, "Report"), "*", SearchOption.AllDirectories))
+                .ToList()
+                .ForEach(f =>
+                {
+                    var bytes = File.ReadAllBytes(f);
+                    var obj = ParseJsonStr(bytes);
+                    var formatted = obj.ToString(Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText(f, formatted);
+                });
+        }
+
+        private static JObject ParseJsonStr(byte [] bytes)
+        {
+            var encodingsToTry = new[] { unicode, Encoding.UTF8 };
+            foreach (var enc in encodingsToTry)
+            {
+                try 
+                {
+                    return JObject.Parse(enc.GetString(bytes));
+                }
+                catch { }
+            }
+            throw new Exception("Unknown encoding or error in json string");
         }
     }
 }
