0830efbe7229bfe2096a9a335f93b9eca53b28d7
Antonio <antonio@querystorm.com>
2022-01-18 10:09:50 +0100

    Assume current folder if 2nd parameter not supplied

--- Program.cs
+++ Program.cs
@@ -9,10 +9,20 @@
             switch (operation)
             {
                 case "pack":
-                    engine.Pack(args[1], args[2]);
+                    if (args.Count() == 3)
+                        engine.Pack(args[1], args[2]);
+                    else if (args.Count() == 2)
+                        engine.Pack(Environment.CurrentDirectory, args[1]);
+                    else
+                        throw new ArgumentException();
                     break;
                 case "unpack":
+                    if(args.Count() == 3)
                     engine.Extract(args[1], args[2]);
+                    else if(args.Count() == 2)
+                        engine.Extract(args[1], Environment.CurrentDirectory);
+                    else
+                        throw new ArgumentException();
                     break;
             }
         }
