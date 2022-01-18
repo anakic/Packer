b58b84468639381c4175af3218c9f77a7cad39d0
Antonio <antonio@querystorm.com>
2022-01-18 10:07:46 +0100

    Add OrderArraysStep

---
+++ Steps/OrderArraysStep.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using Packer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packer.Steps
+{
+    internal class OrderArraysStep : StepBase
+    {
+        public override void ToHumanReadable(RepositoryModel model)
+        {
+            OrderArray(model.DataModelSchemaFile!.JObj.SelectToken("model.relationships")!, "fromTable", "fromColumn", "toTable", "toColumn");
+        }
+
+        private void OrderArray(JToken array, params string [] properties)
+        {
+            var ordered = array.OrderBy(x => x[properties.First()]);
+            foreach (var property in properties.Skip(1))
+                ordered = ordered.ThenBy(x => x[property]);
+            array.Replace(new JArray(ordered));
+        }
+    }
+}
