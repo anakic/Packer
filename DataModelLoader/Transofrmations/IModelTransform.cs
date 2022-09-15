d6938c2df4496a12d088bdb58ceabf932f702974
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-09-15 15:10:25 +0200

    more transforms

--- DataModelLoader/Transofrmations/IModelTransform.cs
+++ DataModelLoader/Transofrmations/IModelTransform.cs
@@ -1,43 +1,10 @@
 ﻿using Microsoft.AnalysisServices.Tabular;
-using Newtonsoft.Json.Linq;
-using System.Data.Common;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
 
 namespace DataModelLoader.Transofrmations
 {
     public interface IDataModelTransform
     {
         Database Transform(Database database);
-    }
-
-    public class PullUpExpressions : IDataModelTransform
-    {
-        public Database Transform(Database database)
-        {
-            var mPartitionSources = database.Model
-                .Tables
-                .SelectMany(t => t.Partitions)
-                .Where(p => p.SourceType == PartitionSourceType.M)
-                .Select(p => p.Source)
-                .OfType<MPartitionSource>()
-                .ToList();
-
-            foreach (var source in mPartitionSources)
-            {
-                var tableName = source.Partition.Table.Name;
-                var expression = source.Expression;
-
-                database.Model.Expressions.Add(new NamedExpression() { Kind = ExpressionKind.M, Name = tableName, Expression = expression });
-
-                // replace original query with pointer to shared expression
-                source.Expression = $@"let
-	Source = #""{tableName}""
-in
-  Source";
-            }
-
-            return database;
-        }
     }
 }
---
+++ DataModelLoader/Transofrmations/StripCulturesTransform.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AnalysisServices.Tabular;
+
+namespace DataModelLoader.Transofrmations
+{
+    public class StripCulturesTransform : IDataModelTransform
+    {
+        public Database Transform(Database database)
+        {
+            database.Model.Cultures.Clear();
+            return database;
+        }
+    }
+}
