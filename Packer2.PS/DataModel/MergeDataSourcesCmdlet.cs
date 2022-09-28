75e9fa91c3553c3329c0390c5a352bad384e77f1
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-09-28 16:10:50 +0200

    MergeDataSourcesCmdlet

---
+++ Packer2.PS/DataModel/MergeDataSourcesCmdlet.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AnalysisServices.Tabular;
+using Packer2.Library;
+using Packer2.Library.DataModel.Transofrmations;
+using System.Management.Automation;
+
+namespace Packer2.PS.DataModel
+{
+    [Cmdlet("Merge", "DataSources")]
+    [OutputType(typeof(Database))]
+    public class MergeDataSourcesCmdlet : PSCmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public Database Database { get; set; }
+
+        [Parameter(Mandatory = true, Position = 0)]
+        [Alias("ds")]
+        public string DataSourcesFilePath { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            new MergeDataSourcesTransform(new LocalTextFile(Path.Combine(SessionState.Path.CurrentLocation.Path, DataSourcesFilePath))).Transform(Database);
+            WriteObject(Database);
+        }
+    }
+}
