435be1b2a84976f0693d81cce9882ca8b531c6b0
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-09-28 16:09:06 +0200

    ResolvePasswordsCmdlet

--- Packer2.Library/DataModel/Transofrmations/PasswordResolveTransform.cs
+++ Packer2.Library/DataModel/Transofrmations/PasswordResolveTransform.cs
@@ -3,14 +3,14 @@ using System.Text.RegularExpressions;
 
 namespace Packer2.Library.DataModel.Transofrmations
 {
-    internal class PasswordResolveTransform : IDataModelTransform
+    public class PasswordResolveTransform : IDataModelTransform
     {
         public Database Transform(Database database)
         {
             foreach (var cred in database.Model.DataSources.OfType<StructuredDataSource>().Select(ds => ds.Credential))
             {
                 var pwd = cred.Password;
-                var m = Regex.Match("{asdf}", @"{(?'name'[^}]+)}");
+                var m = Regex.Match(pwd, @"{(?'name'[^}]+)}");
                 if (m.Success)
                 {
                     var value = Environment.GetEnvironmentVariable(m.Groups["name"].Value);
