8af2b984123eeb1d3432e8b30b3bf8e27bba3a32
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-09-28 09:46:05 +0200

    Add: password resolve data-model-transofrm

---
+++ Packer2.Library/DataModel/Transofrmations/PasswordResolveTransform.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AnalysisServices.Tabular;
+using System.Text.RegularExpressions;
+
+namespace Packer2.Library.DataModel.Transofrmations
+{
+    internal class PasswordResolveTransform : IDataModelTransform
+    {
+        public Database Transform(Database database)
+        {
+            foreach (var cred in database.Model.DataSources.OfType<StructuredDataSource>().Select(ds => ds.Credential))
+            {
+                var pwd = cred.Password;
+                var m = Regex.Match("{asdf}", @"{(?'name'[^}]+)}");
+                if (m.Success)
+                {
+                    var value = Environment.GetEnvironmentVariable(m.Groups["name"].Value);
+                    if (value != null)
+                    {
+                        cred.Password = value;
+                    }
+                }
+            }
+            return database;
+        }
+    }
+}
