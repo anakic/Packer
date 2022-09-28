42a4f751ea58e8c753dacf1e85c416bcb242071d
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-09-28 16:09:41 +0200

    DataModelStoreHelper fix conn str being treated as file path

--- Packer2.PS/DataModel/DataModelStoreHelper.cs
+++ Packer2.PS/DataModel/DataModelStoreHelper.cs
@@ -21,22 +21,22 @@ namespace Packer2.PS.DataModel
 
         public static IDataModelStore GetDataModelStore(string currentPath, string? location)
         {
-            string combinedPath = currentPath;
-            if (!string.IsNullOrEmpty(location))
-                combinedPath = Path.Combine(currentPath, location); //if path is already rooted it will ignore the first arg
-
             IDataModelStore store;
-            if (Path.IsPathRooted(combinedPath))
+            try
             {
+                var connStrBuilder = new SqlConnectionStringBuilder(location);
+                store = new SSASDataModelStore(connStrBuilder.DataSource, connStrBuilder.InitialCatalog);
+            }
+            catch
+            {
+                string combinedPath = currentPath;
+                if (!string.IsNullOrEmpty(location))
+                    combinedPath = Path.Combine(currentPath, location); //if path is already rooted it will ignore the first arg
+
                 if (Path.HasExtension(combinedPath))
                     store = new BimDataModelStore(new LocalTextFile(combinedPath));
                 else
                     store = new FolderModelStore(combinedPath);
-            }
-            else
-            {
-                var connStrBuilder = new SqlConnectionStringBuilder(location);
-                store = new SSASDataModelStore(connStrBuilder.DataSource, connStrBuilder.InitialCatalog);
             }
 
             return store;
