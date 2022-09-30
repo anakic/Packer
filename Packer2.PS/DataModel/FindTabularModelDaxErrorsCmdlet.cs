f8fc725ce0f8120001d31e19da01d8c6d957506d
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-09-30 14:19:32 +0200

    minor

--- Packer2.PS/DataModel/FindTabularModelDaxErrorsCmdlet.cs
+++ Packer2.PS/DataModel/FindTabularModelDaxErrorsCmdlet.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AnalysisServices.Tabular;
-using Packer2.Library.DataModel.Transofrmations;
+﻿using Packer2.Library.DataModel.Transofrmations;
 using System.Management.Automation;
 
 namespace Packer2.PS.DataModel
