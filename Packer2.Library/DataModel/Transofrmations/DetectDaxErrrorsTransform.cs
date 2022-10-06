e319c34e189698d83ddbc81823903f6fc0f2a3f5
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-10-06 14:51:32 +0200

    Detecting dax errors transform (currently relies on Server connection)
    ...in the future might rely on a dax parser (via antlr)

--- Packer2.Library/DataModel/Transofrmations/DetectDaxErrrorsTransform.cs
+++ Packer2.Library/DataModel/Transofrmations/DetectDaxErrrorsTransform.cs
@@ -13,13 +13,24 @@ namespace Packer2.Library.DataModel.Transofrmations
 
         public Database Transform(Database database)
         {
-            var columnErrorMessages = database.Model.Tables.SelectMany(c => c.Columns).Select(c => c.ErrorMessage);
-            var measureErrorMessages = database.Model.Tables.SelectMany(t => t.Measures).Select(m => m.ErrorMessage);
+            if (database.Server == null)
+                throw new Exception("Dax validation currently only available for models that are connected to an SSAS database.");
+
+            var columnErrorMessages = database.Model.Tables.SelectMany(c => c.Columns).Where(c => !string.IsNullOrEmpty(c.ErrorMessage)).Select(c => $"{GetName(c.Parent)}[{c.Name}]: {c.ErrorMessage})");
+            var measureErrorMessages = database.Model.Tables.SelectMany(t => t.Measures).Where(m => !string.IsNullOrEmpty(m.ErrorMessage)).Select(m => $"{GetName(m.Parent)}[{m.Name}]: {m.ErrorMessage})");
 
             foreach (var em in columnErrorMessages.Concat(measureErrorMessages))
                 reportError(em);
 
             return database;
+        }
+
+        private string GetName(MetadataObject metadataObject)
+        {
+            if (metadataObject is NamedMetadataObject nmo)
+                return nmo.Name;
+            else
+                return metadataObject.ToString();
         }
     }
 }
