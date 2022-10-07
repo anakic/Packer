059b4b41f51163804a51df47ffc7f061d429d1b3
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-10-07 10:46:07 +0200

    logging: commandlets - instantiating model stores

--- Packer2.PS/StoreCmdletBase.cs
+++ Packer2.PS/StoreCmdletBase.cs
@@ -4,30 +4,44 @@ using Packer2.Library;
 using Packer2.Library.DataModel;
 using System.Data.SqlClient;
 using System.Management.Automation;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
 
 namespace Packer2.PS
 {
     public abstract class StoreCmdletBase : PSCmdlet
     {
+        ILogger<StoreCmdletBase> logger;
+        public StoreCmdletBase()
+        {
+            logger = this.CreateLogger<StoreCmdletBase>();
+        }
+
         protected IModelStore<PowerBIReport> GetReportStore(string? path, bool checkExists = false)
         {
             string currentPath = SessionState.Path.CurrentLocation.Path;
             string combinedPath = currentPath;
             if (!string.IsNullOrEmpty(path))
+            {
                 combinedPath = Path.Combine(currentPath, path); //if path is already rooted it will ignore the first arg
+            }
 
+            IModelStore<PowerBIReport> store;
             if (Path.HasExtension(combinedPath))
             {
                 if (checkExists && !File.Exists(combinedPath))
                     throw new Exception($"Report file not found at path {combinedPath}");
-                return new PBIArchiveStore(combinedPath);
+                store = new PBIArchiveStore(combinedPath);
+                logger = this.CreateLogger<StoreCmdletBase>();
+                logger.LogTrace("Created .pbit/.pbix report model store '({location})'", combinedPath);
             }
             else
             {
                 if(checkExists && !Directory.Exists(combinedPath))
                     throw new Exception($"Report folder not found at path {combinedPath}");
-                return new ReportFolderStore(combinedPath);
+                store = new ReportFolderStore(combinedPath);
+                logger.LogTrace("Created folder report model store '({location})'", combinedPath);
             }
+            return store;
         }
 
         protected ILogger<T> CreateLogger<T>()
@@ -41,6 +55,7 @@ namespace Packer2.PS
             {
                 var connStrBuilder = new SqlConnectionStringBuilder(location);
                 store = new SSASDataModelStore(connStrBuilder.DataSource, connStrBuilder.InitialCatalog, true, CreateLogger<SSASDataModelStore>());
+                logger.LogTrace("Created SSAS data model store '({location})'", location);
             }
             catch
             {
@@ -58,13 +73,18 @@ namespace Packer2.PS
                         if (reportModel.DataModelSchemaFile == null)
                             throw new ArgumentException($"Cannot read data model from pbit file at {combinedPath}. This report does not have an integrated data model.");
                         store = new BimDataModelStore(new MemoryFile(reportModel.DataModelSchemaFile.ToString()));
+                        logger.LogTrace("Created .pbit file data model store '({location})'", location);
                     }
                     else
+                    {
                         store = new BimDataModelStore(new LocalTextFile(combinedPath));
+                        logger.LogTrace("Created .bim file data model store '({location})'", location);
+                    }
                 }
                 else
                 {
-                    store = new FolderModelStore(combinedPath);
+                    store = new FolderModelStore(combinedPath, CreateLogger<FolderModelStore>());
+                    logger.LogTrace("Created folder data model store '({location})'", location);
                 }
             }
 
