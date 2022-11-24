45a9690dd89cf47960d7fb70807b517c41aa928e
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-11-24 15:44:50 +0100

    Refactoring untuffjson step
    This makes it easier to specify what to stuff/unstuff

--- Packer2.Library/Report/Stores/Folder/Transforms/UnstuffTransform.cs
+++ Packer2.Library/Report/Stores/Folder/Transforms/UnstuffTransform.cs
@@ -5,29 +5,49 @@ namespace Packer2.Library.Report.Stores.Folder.Transforms
 {
     class UnstuffTransform : IJObjTransform
     {
+        string[] selectorsObj = new[] { "..config" };
+        string[] selectorsArr = new[] { "pods[*].parameters", "..filters" };
+
         public void Restore(JObject obj)
         {
-            foreach (var configJObj in obj.SelectTokens("..#config").ToArray())
-                configJObj.Parent!.Replace(new JProperty("config", configJObj.ToString(Formatting.None)));
-
-            foreach (var filtersJArr in obj.SelectTokens("..#filters").ToArray())
-                filtersJArr.Parent!.Replace(new JProperty("filters", filtersJArr.ToString(Formatting.None)));
+            foreach (string selector in selectorsObj.Union(selectorsArr))
+                Restore(obj, selector);
         }
 
         public void Transform(JObject obj)
         {
-            var tokens = obj.SelectTokens("..config").Where(t => t?.Type == JTokenType.String).ToArray();
+            foreach (string selector in selectorsObj)
+                TransformObj(obj, selector);
+
+            foreach (string selector in selectorsArr)
+                TransformArr(obj, selector);
+        }
+
+        private void Restore(JObject obj, string path)
+        {
+            var pathAdjusted = path.Insert(path.LastIndexOf(".") + 1, "#");
+
+            foreach (var filtersJArr in obj.SelectTokens(pathAdjusted).ToArray())
+                filtersJArr.Parent!.Replace(new JProperty(pathAdjusted.Substring(1 + pathAdjusted.LastIndexOf("#")), filtersJArr.ToString(Formatting.None)));
+        }
+
+        private void TransformObj(JObject obj, string path)
+        {
+            var tokens = obj.SelectTokens(path).Where(t => t?.Type == JTokenType.String).ToArray();
             foreach (var tok in tokens)
             {
                 var jObj = JObject.Parse(tok.Value<string>()!);
-                tok.Parent!.Replace(new JProperty("#config", jObj));
+                tok.Parent!.Replace(new JProperty("#" + path.Substring(path.LastIndexOf('.') + 1), jObj));
             }
+        }
 
-            var filtersTokens = obj.SelectTokens("..filters").Where(t => t?.Type == JTokenType.String).ToArray();
-            foreach (var tok in filtersTokens)
+        private void TransformArr(JObject obj, string path)
+        {
+            var tokens = obj.SelectTokens(path).Where(t => t?.Type == JTokenType.String).ToArray();
+            foreach (var tok in tokens)
             {
-                var jArr = JArray.Parse(tok.Value<string>()!).OfType<JObject>().ToArray();
-                tok.Parent!.Replace(new JProperty("#filters", jArr));
+                var jArr = JArray.Parse(tok.Value<string>()!);
+                tok.Parent!.Replace(new JProperty("#" + path.Substring(path.LastIndexOf('.') + 1), jArr));
             }
         }
     }
