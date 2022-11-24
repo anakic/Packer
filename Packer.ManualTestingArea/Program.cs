c38e3aeb07f5f9b42038cf5bc4a4b795559c7f3c
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-11-24 14:12:07 +0100

    some testing code

--- Packer.ManualTestingArea/Program.cs
+++ Packer.ManualTestingArea/Program.cs
@@ -1,7 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DataModelLoader.Report;
 using Microsoft.Extensions.Logging;
+using Microsoft.InfoNav.Data.Contracts.Internal;
+using Newtonsoft.Json;
+using Packer2.Library.MinifiedQueryParser.QueryTransforms;
+using Packer2.Library.Report.QueryTransforms.Antlr;
 using Packer2.Library.Report.Stores.Folder;
+using Packer2.Library.Tools;
+using System.Diagnostics;
 
 using var loggerFactory = LoggerFactory.Create(builder =>
 {
@@ -12,6 +18,12 @@ using var loggerFactory = LoggerFactory.Create(builder =>
         .AddConsole();
 });
 
+string folder = @"C:\TEst\raspakirano_sabp";
+string raspakiranoPbix = @"C:\TEst\raspakirano_sabp.pbix";
+string wf3_ssas = @"C:\TEst\wf3_ssas.pbix";
+string wf3pbit = @"C:\Users\AntonioNakic-Alfirev\OneDrive - SSG Partners Limited\Desktop\ward_flow3.pbit";
+string sjvpbix = @"C:\TEst\samo_jedan_visual.pbix";
+
 //string str = @"{from d in Ward
 //orderby d.TypeKind ascending
 //select d.TypeKind }.TypeKind";
@@ -21,11 +33,38 @@ using var loggerFactory = LoggerFactory.Create(builder =>
 //var p1 = new QueryParser(loggerFactory.CreateLogger<QueryParser>());
 //var q = p1.ParseExpression(str2);
 
-var rfs = new PBIArchiveStore(@"C:\Users\AntonioNakic-Alfirev\OneDrive - SSG Partners Limited\Desktop\ward_flow3.pbit");
+//var rfs = new PBIArchiveStore(@"C:\Users\AntonioNakic-Alfirev\OneDrive - SSG Partners Limited\Desktop\ward_flow3.pbit");
+//var report = rfs.Read();
+
+//var folderStore = new ReportFolderStore(@"c:\test\wf3_orig");
+//folderStore.Save(report);
+
+// 1. procitaj
+var rfs = new PBIArchiveStore(@"C:\Users\AntonioNakic-Alfirev\Downloads\SABP Mental Health Flow Tool - v1.2.17 Development Master.pbix");
 var report = rfs.Read();
 
-var folderStore = new ReportFolderStore(@"c:\test\wf3_orig");
+//// 2. spremi minificirano
+var folderStore = new ReportFolderStore(folder);
+folderStore.EnableMinification();
 folderStore.Save(report);
+
+//// 3. deminificiraj u folderu
+report = folderStore.Read();
+folderStore.DisableMinification();
+folderStore.Save(report);
+
+// 4. procitaj tako deminificirano i zapakiraj
+report = folderStore.Read();
+var rfs2 = new PBIArchiveStore(raspakiranoPbix);
+rfs2.Save(report);
+
+// 5. upali i testiraj
+Process.Start("explorer.exe", raspakiranoPbix);
+
+//report = folderStore.Read();
+
+//var rfs2 = new PBIArchiveStore(@"C:\TEst\samo_jedan_visual2.pbix");
+//rfs2.Save(report);
 //var report = folderStore.Read();
 
 //var rfs2 = new PBIArchiveStore(@"c:\test\sjv.pbix");
