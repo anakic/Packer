fca9644fea5fa4d57c6d0592ed2fe85171b4e696
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-11-24 14:06:31 +0100

    Removing unused files

--- Packer2.Library/Report/Decompiled/AxisTypes.cs
+++
@@ -1,8 +0,0 @@
-namespace Microsoft.InfoNav.Explore.VisualContracts;
-
-internal static class AxisTypes
-{
-	internal const string Categorical = "Categorical";
-
-	internal const string Scalar = "Scalar";
-}
--- Packer2.Library/Report/Decompiled/FilterType.cs
+++
@@ -1,8 +0,0 @@
-namespace Microsoft.InfoNav.Explore.VisualContracts;
-
-internal sealed class FilterType
-{
-	internal static string Categorical => "Categorical";
-
-	internal static string Advanced => "Advanced";
-}
