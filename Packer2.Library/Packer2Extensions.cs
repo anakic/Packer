5b3e5d8f9a635452203676a0dde2b3093ebd92a7
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-12-05 13:06:08 +0100

    ext methods for reading/saving ssas model

--- Packer2.Library/Packer2Extensions.cs
+++ Packer2.Library/Packer2Extensions.cs
@@ -20,9 +20,11 @@ namespace Packer2.Library
         public static Database LoadFromFolder(string path)
             => new FolderDatabaseStore(new LocalFileSystem(path)).Read();
 
+        public static Database LoadFromSSAS(string connectionString)
+            => new SSASDataModelStore(connectionString).Read();
+
         public static Database LoadFromSSAS(string server, string database)
             => new SSASDataModelStore(server, database).Read();
-
 
         public static Database ExtractDataSourcesOnly(this Database db)
             => new ExportDataSourcesTransform().Transform(db);
@@ -45,6 +47,9 @@ namespace Packer2.Library
 
         public static void SaveToFolder(this Database db, string folderPath)
             => new FolderDatabaseStore(folderPath).Save(db);
+
+        public static void SaveToSSAS(this Database db, string server, string database)
+            => new SSASDataModelStore(server, database).Save(db);
 
         public static void SaveToBimFile(this Database db, string filePath)
             => new BimDataModelStore(new TextFileStore(filePath)).Save(db);
