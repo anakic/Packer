ce3e3b5b6785f70599104441b6af3c78cd502c6a
Antonio Nakic-Alfirevic <antonio@querystorm.com>
2022-12-05 13:16:51 +0100

    Add SwitchPbiReportToLocalDataModel cmdlet

---
+++ Packer2.PS/Report/SwitchPbiReportToLocalDataModel.cs
@@ -0,0 +1,18 @@
+﻿using DataModelLoader.Report;
+using System.Management.Automation;
+
+namespace Packer2.PS.Report
+{
+    [Cmdlet(VerbsCommon.Switch, "PbiReportToLocalDataModel")]
+    [OutputType(typeof(PowerBIReport))]
+    public class SwitchPbiReportToLocalDataModel : StoreCmdletBase
+    {
+        [Parameter(ValueFromPipeline = true)]
+        public PowerBIReport Report { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(Report.SwitchToLocalDataModel());
+        }
+    }
+}
